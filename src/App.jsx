ca23a30b15f1d49ab619c65ec6f9c3bcee3e08ef
MalaikaAliGitHub <malaikaa7474@gmail.com>
2026-07-04 16:13:38 -0700

    fix: clean unused admin imports to fix vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,11 @@ import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 
-import Login from "./admin/Login";
-import Dashboard from "./admin/Dashboard";
-import ManageProjects from "./admin/ManageProjects";
-
 import PublicLayout from "./layouts/PublicLayout";
-import AdminLayout from "./layouts/AdminLayout";
-
-import AdminRoute from "./routes/AdminRoute";
 
 function App() {
   return (
     <BrowserRouter>
-
       <Routes>
 
         {/* =========================
@@ -32,29 +24,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Route>
 
-        {/* =========================
-            LOGIN (NO PROTECTION)
-        ========================== */}
-        <Route path="/login" element={<Login />} />
-
-        {/* =========================
-            ADMIN PROTECTED ROUTES
-        ========================== */}
-        <Route
-          element={
-            <AdminRoute>
-              <AdminLayout />
-            </AdminRoute>
-          }
-        >
-
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/projects" element={<ManageProjects />} />
-
-        </Route>
-
       </Routes>
-
     </BrowserRouter>
   );
 }
